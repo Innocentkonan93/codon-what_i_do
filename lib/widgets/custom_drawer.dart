a4437903c6f41e44aa348711e332b75cdc5f11c4
Josco <josco@MacBook-Air-de-Josco.local>
2022-06-01 22:14:11 +0200

    01/06/2022

--- lib/widgets/custom_drawer.dart
+++ lib/widgets/custom_drawer.dart
@@ -5,6 +5,7 @@ import 'package:flutter/services.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_svg/svg.dart';
 import 'package:google_fonts/google_fonts.dart';
+import 'package:share_plus/share_plus.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 import 'package:zoknot/configs/theme.dart';
 
@@ -106,6 +107,18 @@ class _CustomDrawerState extends State<CustomDrawer> {
                         style: Theme.of(context).textTheme.headline5,
                       ),
                       onTap: () {
+                        Navigator.pop(context);
+                        if (Platform.isIOS) {
+                          Share.share(
+                            "Zoknot une superbe application de note essai-le\nhttps://apps.apple.com/us/app/zoknot/id1622896004",
+                            subject: "Partager Zoknot",
+                          );
+                        } else {
+                          Share.share(
+                            "Zoknot une superbe application de note essai-le\nhttps://play.google.com/store/apps/details?id=com.story.zoknot",
+                            subject: "Partager Zoknot",
+                          );
+                        }
                       },
                     ),
                     ListTile(
@@ -132,7 +145,7 @@ class _CustomDrawerState extends State<CustomDrawer> {
                 ),
                 const Spacer(),
                 const SafeArea(
-                  child:  Padding(
+                  child: Padding(
                     padding: EdgeInsets.all(8.0),
                     child: Text("V 1.2.43"),
                   ),
